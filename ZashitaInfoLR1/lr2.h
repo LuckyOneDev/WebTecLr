d82e56058e59c7bcba67b8dc972f5847b6bdb1d1
Ivan Mokshin <mokshinin@mail.ru>
2023-11-28 21:21:24 +0300

    ff

--- ZashitaInfoLR1/lr2.h
+++ ZashitaInfoLR1/lr2.h
@@ -1,74 +1,66 @@
 ﻿#pragma once
 #include <iostream>
-#include <mpir.h>
+#include <tuple>
+#include <mpirxx.h>
 
 using namespace std;
 
 // Function to generate a random prime number of given bit length
-void generate_prime(mpz_t random_number, int bit_length) {
+mpz_class generate_prime(int bit_length) {
+    mpz_class random_number;
     // Initialize a random number generator
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
+    gmp_randclass r(gmp_randinit_default);
+    r.seed(time(NULL));
     // Generate a random number of the given bit length
-    mpz_init(random_number);
-    gmp_randseed_ui(state, time(NULL));
-    mpz_urandomb(random_number, state, bit_length);
+    random_number = r.get_z_bits(bit_length);
     // Find the next prime number
-    mpz_nextprime(random_number, random_number);
+    mpz_nextprime(random_number.get_mpz_t(), random_number.get_mpz_t());
     // Clear the random number if it's not prime
-    while (!mpz_probab_prime_p(random_number, 25)) {
-        mpz_clear(random_number);
-        mpz_init(random_number);
-        gmp_randseed_ui(state, time(NULL));
-        mpz_urandomb(random_number, state, bit_length);
-        mpz_nextprime(random_number, random_number);
+    while (!mpz_probab_prime_p(random_number.get_mpz_t(), 25)) {
+        random_number = r.get_z_bits(bit_length);
+        mpz_nextprime(random_number.get_mpz_t(), random_number.get_mpz_t());
     }
+    return random_number;
 }
 
+struct key_struct {
+    mpz_class e;
+    mpz_class n;
+    mpz_class d;
+};
+
 // Function to calculate the public key for RSA encryption
-void calculate_public_key(mpz_t n, mpz_t p, mpz_t q) {
-    mpz_init(n);
-    mpz_mul(n, p, q);
-
+key_struct get_keys(mpz_class p, mpz_class q) {
+    key_struct keys;
+    mpz_class n = p * q;
     // Calculate ϕ(n) = (p - 1) * (q - 1)
-    mpz_t phi_n;
-    mpz_init(phi_n);
-    mpz_sub_ui(p, p, 1);
-    mpz_sub_ui(q, q, 1);
-    mpz_mul(phi_n, p, q);
-
+    mpz_class phi_n = (p - 1) * (q - 1);
+        
     // Calculate e such that 1 < e < ϕ(n) and gcd(e, ϕ(n)) = 1
-    mpz_t e;
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
+    gmp_randclass r(gmp_randinit_default);
+    mpz_class e, gcd;
+    do {
+        e = r.get_z_range(phi_n - 1) + 1;
+        mpz_gcd(gcd.get_mpz_t(), e.get_mpz_t(), phi_n.get_mpz_t());
+    } while (gcd != 1);
 
-    mpz_init(e);
-    mpz_urandomm(e, state, n);
-    mpz_t gcd;
-    mpz_init(gcd);
-    mpz_gcd(gcd, e, phi_n);
-    while (mpz_get_ui(gcd) != 1) {
-        mpz_clear(e);
-        mpz_init(e);
-        mpz_urandomm(e, state, phi_n);
-        mpz_gcd(gcd, e, phi_n);
-    }
-
-    // Calculate d such that e * d ≡ 1 (mod ϕ(n))
-    mpz_t d;
-    mpz_init(d);
-    mpz_invert(d, e, phi_n);
+    mpz_class d;
+    mpz_invert(d.get_mpz_t(), e.get_mpz_t(), phi_n.get_mpz_t());
+    keys.d = d;
+    keys.e = e;
+    keys.n = n;
+    return keys;
 }
 
 // Function to encrypt a message using RSA encryption
-void encrypt(mpz_t c, mpz_t message, mpz_t n, mpz_t e) {
+mpz_class encrypt(mpz_class message, key_struct keys) {
     // Calculate c = m^e (mod n)
-    mpz_init(c);
+    
     mpz_powm(c, message, e, n);
 }
 
 // Function to decrypt a message using RSA encryption
-void decrypt(mpz_t message, mpz_t ciphertext, mpz_t n, mpz_t d) {
+mpz_class decrypt(mpz_class message, key_struct keys) {
     // Calculate m = c^d (mod n)
     mpz_init(message);
     mpz_powm(message, ciphertext, d, n);
@@ -76,35 +68,31 @@ void decrypt(mpz_t message, mpz_t ciphertext, mpz_t n, mpz_t d) {
 
 int lr2() {
     // Generate two random prime numbers of 1024 bits each
-    mpz_t p, q;
-    generate_prime(p, 1024);
-    generate_prime(q, 1024);
+    mpz_class p, q, n, e, d, phi;
+    p = generate_prime(1024);
+    q = generate_prime(1024);
 
     // Calculate the public key (n, e)
-    mpz_t n;
-    calculate_public_key(n, p, q);
-    mpz_t e;
-    calculate_public_key(e, p, q);
+    auto pub = get_keys(p, q);
 
     // Calculate the value of d
-    mpz_t d;
-    mpz_t phi;
-    mpz_init(d);
-    mpz_init(phi);
+    mpz_class d;
+    mpz_class phi;
     mpz_sub_ui(p, p, 1);
     mpz_sub_ui(q, q, 1);
     mpz_mul(phi, p, q);
-    mpz_invert(d, e, phi);
+    mpz_invert(d, pub, phi);
 
     // Generate a message to encrypt
     mpz_t message;
     mpz_init(message);
-    mpz_set_str(message, "Hello world!", 10);
+    mpz_set_str(message, "15211\0", 10);
 
     // Encrypt the message
     mpz_t ciphertext;
     mpz_init(ciphertext);
     encrypt(ciphertext, message, n, e);
+    printf("Ciphertext: %s\n", mpz_get_str(NULL, 10, ciphertext));
 
     // Decrypt the message
     mpz_t decrypted_message;
